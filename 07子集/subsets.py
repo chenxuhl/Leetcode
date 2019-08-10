bb7adf451b77fbfcfd1d153925ba57bcd23bb1f6
chenxu2936@gmail.com <chenxu2936@gmail.com>
2019-08-10 22:31:11 +0800

    add subsets file

---
+++ 07子集/subsets.py
@@ -0,0 +1,17 @@
+#²Î¿¼£ºhttp://www.voidcn.com/article/p-osxncqlg-brs.html
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.results = []
+        self.search(sorted(nums), [], 0)
+        return self.results
+    
+    def search(self, nums, S, index):
+        if index == len(nums):
+            self.results.append(S)
+            return
+        self.search(nums, S + [nums[index]], index + 1)
+        self.search(nums, S, index + 1)
+        
+            
+            
+        
