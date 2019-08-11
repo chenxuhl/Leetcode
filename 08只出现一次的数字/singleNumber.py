f4c9c0b88c102d8d6aae0fee76b75090be760480
chenxu2936@gmail.com <chenxu2936@gmail.com>
2019-08-11 22:22:36 +0800

    add singleNumber file

---
+++ 08只出现一次的数字/singleNumber.py
@@ -0,0 +1,23 @@
+#v1£ºself-thinking
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        n = len(nums)
+        result = None
+        for i in range(0, n-1, 2):
+            if nums[i] != nums[i+1]:
+                result = nums[i]
+                break
+        if result == None:
+            result = nums[n-1]
+        return result
+        
+
+#v2,reference£ºhttps://github.com/grandyang/leetcode/issues/136
+#a¨’b¨’a=b
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        r = 0
+        for num in nums:
+            r ^= num
+        return r
