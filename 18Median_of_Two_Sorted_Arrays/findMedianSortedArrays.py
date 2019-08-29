0a01ad2aa2a91d33dc321b11532739996303b8e1
chenxu2936@gmail.com <chenxu2936@gmail.com>
2019-08-29 21:37:36 +0800

    add 18_th

---
+++ 18Median_of_Two_Sorted_Arrays/findMedianSortedArrays.py
@@ -0,0 +1,73 @@
+#4.寻找两个有序数组的中位数
+"""
+题目描述：
+
+给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+
+请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+
+你可以假设 nums1 和 nums2 不会同时为空。
+
+"""
+#示例1：
+"""
+nums1 = [1, 3]
+nums2 = [2]
+
+则中位数是 2.0
+"""
+#示例2：
+"""
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+则中位数是 (2 + 3)/2 = 2.5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+#解题思路：
+"""
+这题没思路，主要难点：设计按照规定时间复杂度，故一开始想的先融合两个数组，再找中位数的方法显然不可取。
+主要参考：url1：https://zhuanlan.zhihu.com/p/39129143   url2：https://github.com/grandyang/leetcode/issues/4
+"""
+#python3
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        m, n = len(nums1), len(nums2)
+        if m > n:
+            nums1, nums2, m, n = nums2, nums1, n, m
+        if n == 0:
+            raise ValueError
+
+        imin, imax, half_len = 0, m, (m + n + 1) // 2
+        while imin <= imax:
+            i = (imin + imax) // 2
+            j = half_len - i
+            if i < m and nums2[j-1] > nums1[i]:
+                # i is too small, must increase it
+                imin = i + 1
+            elif i > 0 and nums1[i-1] > nums2[j]:
+                # i is too big, must decrease it
+                imax = i - 1
+            else:
+                # i is perfect
+
+                if i == 0: max_of_left = nums2[j-1]
+                elif j == 0: max_of_left = nums1[i-1]
+                else: max_of_left = max(nums1[i-1], nums2[j-1])
+
+                if (m + n) % 2 == 1:
+                    return max_of_left
+
+                if i == m: min_of_right = nums2[j]
+                elif j == n: min_of_right = nums1[i]
+                else: min_of_right = min(nums1[i], nums2[j])
+
+                return (max_of_left + min_of_right) / 2.0
