273cca396fd5e0442db3d1add666d93f9db82005
chenxu2936@gmail.com <chenxu2936@gmail.com>
2019-09-30 21:45:13 +0800

    add 44 and 45-th

---
+++ 44Multiply_Strings/multiply.py
@@ -0,0 +1,43 @@
+#43.字符串相乘
+"""
+题目描述：
+给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，
+它们的乘积也表示为字符串形式。
+
+示例1:
+输入: num1 = "2", num2 = "3"
+输出: "6"
+
+示例2：
+输入: num1 = "123", num2 = "456"
+输出: "56088"
+
+说明：
+1. num1 和 num2 的长度小于110。
+2. num1 和 num2 只包含数字 0-9。
+3. num1 和 num2 均不以零开头，除非是数字 0 本身。
+4. 不能使用任何标准库的大数类型（比如 BigInteger）或直接将输入转换为整数来处理。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/multiply-strings
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+#解题思路：
+"""
+想法比较简单，直接使用python整数和字符串相互转换方法。
+"""
+#python
+#- * - coding: utf-8 - * -
+#Author: JoeyChen
+"""
+执行时间 16ms， 击败99.26%的对手； 内存损耗： 11.7MB
+"""
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return str(int(num1) * int(num2)) #转换成整型相乘，字符串输出
+
