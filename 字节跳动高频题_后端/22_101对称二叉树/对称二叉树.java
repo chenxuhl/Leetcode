688bf0f76ff6e983a2b001424fa2ddcf3ae1770e
xuchen <316423632@qq.com>
2020-07-20 13:53:51 +0800

    提交第22题，二叉树，递归&迭代

---
+++ 字节跳动高频题_后端/22_101对称二叉树/对称二叉树.java
@@ -0,0 +1,102 @@
+/*
+101. 对称二叉树
+给定一个二叉树，检查它是否是镜像对称的。
+
+ 
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ 
+
+进阶：
+
+你可以运用递归和迭代两种方法解决这个问题吗？
+*/
+
+//参考官方答案：递归
+
+//Java
+//Author:Joey
+//Date：2020/07/20
+
+/*
+执行结果：
+执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+内存消耗：37.8 MB, 在所有 Java 提交中击败了35.00%的用户
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {  //难点在于：用到双指针
+        return helper(root, root);        
+    }
+    public boolean helper(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && helper(p.left, q.right) && helper(p.right, q.left);
+    }
+}
+
+//迭代，，用队列（递归改迭代常用方法）
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return helper(root, root);        
+    }
+    public boolean helper(TreeNode u, TreeNode v) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        Queue<TreeNode> p = new LinkedList<TreeNode>();
+
+        q.offer(u);
+        q.offer(v);
+        while (!q.isEmpty()) {
+            u = q.poll();
+            v = q.poll();
+            if (u == null && v == null) continue;
+            if ((u == null || v == null) || (u.val != v.val)) {
+                return false;
+            }
+            q.offer(u.left);
+            q.offer(v.right);
+
+            q.offer(u.right);
+            q.offer(v.left);
+        }
+        return true;
+    }
+}
