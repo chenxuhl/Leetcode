f6d2c605fc88044fc51bb534e8a3d9cba49d0bba
xuchen <316423632@qq.com>
2020-07-27 15:28:05 +0800

    添加解法，栈

--- Leetcode剑指offer/04_06.从尾到头打印链表/从尾到头打印链表.java
+++ Leetcode剑指offer/04_06.从尾到头打印链表/从尾到头打印链表.java
@@ -64,3 +64,33 @@ class Solution {
         res[b] = temp;
     }
 }
+
+//解法二：可以用栈
+
+//时间复杂度: O(n), 空间复杂度：O(n)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] reversePrint(ListNode head) {
+        Stack<ListNode> stack = new Stack<ListNode>();
+        ListNode temp = head;
+        while (temp != null) {
+            stack.push(temp);
+            temp = temp.next;
+        }
+        int size = stack.size();
+        int[] print = new int[size];
+        for (int i = 0; i < size; i++) {
+            print[i] = stack.pop().val;
+        }
+        return print;
+    }
+}
+
