b032fd02b447812e174de7b1892aae069fb564fa
xuchen <316423632@qq.com>
2020-07-28 09:26:07 +0800

    数列 bad

---
+++ LeetCode随机/01_665.非递减数列/非递减数列.java
@@ -0,0 +1,52 @@
+/*
+665. 非递减数列
+给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列。
+
+我们是这样定义一个非递减数列的： 对于数组中所有的 i (0 <= i <= n-2)，总满足 nums[i] <= nums[i + 1]。
+
+ 
+
+示例 1:
+
+输入: nums = [4,2,3]
+输出: true
+解释: 你可以通过把第一个4变成1来使得它成为一个非递减数列。
+示例 2:
+
+输入: nums = [4,2,1]
+输出: false
+解释: 你不能在只改变一个元素的情况下将其变为非递减数列。
+ 
+
+说明：
+
+1 <= n <= 10 ^ 4
+- 10 ^ 5 <= nums[i] <= 10 ^ 5
+*/
+
+//Java
+//Author:Joey
+//Date：2020/07/28
+
+//解题思路: 需要统计nums[i] > nums[i+1]次数
+
+//时间复杂度: O(n), 空间复杂度：O(1)
+
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int n = nums.length;
+        int sign = 0;
+        for (int i = 1; i < n && sign < 2; i++) {
+            if (nums[i-1] <= nums[i]) continue;
+            if  (i >= 2 && nums[i] < nums[i-2]) {
+                nums[i] = nums[i-1];  //i-1 值赋值给i
+            } 
+            else {
+                nums[i-1] = nums[i];  //i值赋值给i-1
+            }
+            sign++;
+        }
+        return sign > 1 ? false : true;
+
+    }
+}
