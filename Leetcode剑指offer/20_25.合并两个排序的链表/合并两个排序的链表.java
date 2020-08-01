03390216305620c009fd3186a1e558331c002ff3
xuchen <316423632@qq.com>
2020-08-01 17:16:46 +0800

    有序链表，not bad

---
+++ Leetcode剑指offer/20_25.合并两个排序的链表/合并两个排序的链表.java
@@ -0,0 +1,52 @@
+/*
+剑指 Offer 25. 合并两个排序的链表
+输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+
+示例1：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+限制：
+
+0 <= 链表长度 <= 1000
+
+注意：本题与主站 21 题相同：https://leetcode-cn.com/problems/merge-two-sorted-lists/
+*/
+
+//Java
+//Author:Joey
+//Date：2020/08/01
+
+/*
+解题思路: 遍历
+*/
+
+
+//时间复杂度: O(n+m), 空间复杂度：O(1)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(0);
+        ListNode cur = res;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;  //注意这里的next
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+        return res.next;
+    }
+}
